added09db5337dbaea0078c46b601a2020500dc8
MalikSabirov <maaliksabirov@gmail.com>
2025-02-21 00:59:47 +0500

    feat: Modify global value and restore it with defer

--- main.go
+++ main.go
@@ -6,6 +6,22 @@ import (
 	"go-crash-course/greetings"
 )
 
+var Global = 5
+
+func ModifyGlobal() {
+	fmt.Println("\nBefore modify: ", Global)
+
+	original := Global
+
+	defer func() {
+		Global = original
+		fmt.Println("After defer restore initial value: ", Global)
+	}()
+
+	Global = 10
+	fmt.Println("After modify: ", Global)
+}
+
 func main() {
 	greetings.SayHello()
 	data_types.WorkWithPrimitives()
@@ -15,4 +31,7 @@ func main() {
 	fmt.Println("Age category: ", data_types.Category(myAge))
 
 	fmt.Println(greetings.GreetByYear(2029))
+
+	ModifyGlobal()
+	println("in main after call ModifyGlobal: ", Global)
 }
